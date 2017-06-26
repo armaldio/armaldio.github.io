4e4b512f87c32c006997f0daa9f083e42dae4d74
Quentin Goinaud <armaldio@gmail.com>
2017-06-27 00:42:13 +0200

    -

--- sw.js
+++ sw.js
@@ -1,39 +1,42 @@
-var CACHE_NAME = 'cache-v1';
+var CACHE_NAME   = 'cache-v1';
 var filesToCache = [
-    "index.html"
+	"index.html"
 ];
 
 self.addEventListener("install", function (event) {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(function (cache) {
-            console.log("Cache opeened");
-            return (cache.addAll(filesToCache));
-        })
-    )
+	/*event.waitUntil(
+	 caches.open(CACHE_NAME).then(function (cache) {
+	 console.log("Cache opeened");
+	 return (cache.addAll(filesToCache));
+	 })
+	 )*/
 });
 
 self.addEventListener("fetch", function (event) {
-    event.respondWith(
-        caches.match(event.request).then(function (response) {
-            if (response)
-                return response;
+	event.respondWith(
+		fetch(event.request)
+	)
+	/*event.respondWith(
+	 caches.match(event.request).then(function (response) {
+	 if (response)
+	 return response;
 
-            var fetchRequest = event.request.clone();
+	 var fetchRequest = event.request.clone();
 
-            return fetch(fetchRequest).then(function (response) {
-                if (!response || response.status !== 200 || response.type !== 'basic') {
-                    return response;
-                }
+	 return fetch(fetchRequest).then(function (response) {
+	 if (!response || response.status !== 200 || response.type !== 'basic') {
+	 return response;
+	 }
 
-                var responseToCache = response.clone();
-                caches.open(CACHE_NAME).then(function (cache) {
-                    cache.put(event.request, responseToCache);
-                });
+	 var responseToCache = response.clone();
+	 caches.open(CACHE_NAME).then(function (cache) {
+	 cache.put(event.request, responseToCache);
+	 });
 
-                return response;
-            })
-        })
-    )
+	 return response;
+	 })
+	 })
+	 )*/
 });
 
 self.addEventListener("activate", function () {
