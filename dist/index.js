0375285500a65e191756d8a9006ad2266f8463f3
Quentin Goinaud <armaldio@gmail.com>
2018-02-16 19:37:01 +0100

    cleanup

--- dist/index.js
+++ dist/index.js
@@ -5,25 +5,6 @@
  */
 
 $(document).ready(function () {
-	/*let miner = new CoinHive.Anonymous('MrpTErm91qOLopimz9WmeVWH54pXnv2Z', {
- 	autoThreads: true,
- 	theme: 'dark',
- 	threads: 1,
- 	throttle: 0.8,
- 	language: 'auto'
- });
- miner.start();*/
-
-	/*let $el = $('#popupMiner');
- 
- $el.on('click', function () {
- 	$el.hide();
- 	miner.stop();
- });
- 
- miner.on('open', function () {
- 	$el.show();
- });*/
 
 	$('.dropdown').dropdown({
 		action: 'hide'
@@ -54,71 +35,5 @@ $(document).ready(function () {
 
 		$('.weuk.modal').modal('show');
 	});
-
-	//AOS.init();
-
-	/*let currentLang = localStorage.getItem("i18nextLng");
- if (currentLang === "fr")
- 	$('#lang-dd').dropdown('set selected', 1);
- else if (currentLang === "en")
- 	$('#lang-dd').dropdown('set selected', 0);
- $('#lang-dd').dropdown('refresh');
- 
- /!**
-  * i18next
-  *!/
- i18next
- 	.use(i18nextXHRBackend)
- 	.use(i18nextBrowserLanguageDetector)
- 	.init({
- 		fallbackLng: 'en',
- 		debug      : false,
- 		backend    : {
- 			loadPath   : 'assets/i18n/{{lng}}/{{ns}}.json',
- 			crossDomain: true
- 		}
- 	}, function (err, t) {
- 		updateContent();
- 	});
- 
- // just set some content and react to language changes
- // could be optimized using vue-i18next, jquery-i18next, react-i18next, ...
- function updateContent () {
- 	for (let i = 0; i < document.querySelectorAll('[data-i18n]').length; i++) {
- 		document.querySelectorAll('[data-i18n]')[i].innerHTML = i18next.t(document.querySelectorAll('[data-i18n]')[i].getAttribute("data-i18n"));
- 	}
- }
- 
- function changeLng (lng) {
- 	i18next.changeLanguage(lng);
- }
- 
- i18next.on('languageChanged', () => {
- 	updateContent();
- });
- 
- $("#language").on("change", () => {
- 
- 	let lang = $('#lang-dd').dropdown('get value');
- 
- 	if (parseInt(lang) === 0) {
- 		i18next.changeLanguage('en');
- 	} else {
- 		i18next.changeLanguage('fr');
- 	}
- 
- });
- 
- /!**
-  * //i18next
-  *!/
- 
- if ("serviceWorker" in navigator) {
- 	navigator.serviceWorker.register('./sw.js').then((registration) => {
- 		console.log("Service worker registered");
- 	}, () => {
- 		console.log("Service worker failed to register");
- 	})
- }*/
 });
 //# sourceMappingURL=index.js.map
